ae9400f3373aba170388dddceae5bb4e8d1442d0
Titouan-C <titouan.chabanon31@gmail.com>
2023-12-06 16:19:04 +0100

    update routes

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Company } from '../company.model';
 import { CompanyService } from '../company.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company-list',
@@ -13,7 +14,7 @@ export class CompanyListComponent implements OnInit, OnDestroy {
   companyList = new Array<Company>();
   subscription: Subscription | undefined;
 
-  constructor(private companyService: CompanyService) { }
+  constructor(private companyService: CompanyService, private router: Router) { }
 
   ngOnInit(): void {
     if (!this.subscription) {
@@ -33,6 +34,10 @@ export class CompanyListComponent implements OnInit, OnDestroy {
   updateList(company: Company): void {
     let indexItem: number = this.companyList.findIndex(c => c.id === company.id);
     this.companyList[indexItem] = { ...this.companyList[indexItem], ...company };
+  }
+
+  goToCompany(company: Company): void {
+    this.router.navigate(['/company', company.id])
   }
 
   ngOnDestroy(): void {
