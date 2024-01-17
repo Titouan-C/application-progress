fb03c7b6dfef374469fe0a73364b0ad36fe1531d
Titouan-C <titouan.chabanon31@gmail.com>
2024-01-17 09:26:19 +0100

    add navbar component & error msgs on company-form

---
+++ src/app/public/public.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NavbarComponent
+  ],
+  exports: [
+    NavbarComponent
+  ]
+})
+export class PublicModule { }
